2769e0e98797bd0a70cb18c62c2064e66ed596ef
aqsaaqeel <aqsa.aqeel17@gmail.com>
2021-09-10 00:29:59 +0530

    feat : added input placeholder

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
 btnTranslate.addEventListener("click", clickEventHandler())
 
-function clickEventHandler() {
+function clickEventHandler(){
     console.log("I was clicked")
+    console.log("input", txtInput.value)
 }
